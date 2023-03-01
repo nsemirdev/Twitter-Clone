2033d8b9a2956a35d405bcb0e768c4fb4dc37b52
nsemirdev <nsemirdev@gmail.com>
2023-03-01 22:05:41 +0300

    initial setup

---
+++ Twitter-Clone/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  Twitter-Clone
+//
+//  Created by Emir Alkal on 1.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
+
