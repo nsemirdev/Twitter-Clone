51da767ed17301979bb2c64e523568a5090f5692
nsemirdev <nsemirdev@gmail.com>
2023-03-01 22:02:45 +0300

    Initial Commit

---
+++ Twitter-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter-Clone
+//
+//  Created by Emir Alkal on 1.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
